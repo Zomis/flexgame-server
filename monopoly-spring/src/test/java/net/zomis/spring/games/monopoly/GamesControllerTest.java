1215c764934277f9cd8170c11e6138aabc4cad71
Simon Forsberg <zomis2k@hotmail.com>
2016-12-18 11:34:38 +0100

    Update annotations in test

--- monopoly-spring/src/test/java/net/zomis/spring/games/monopoly/GamesControllerTest.java
+++ monopoly-spring/src/test/java/net/zomis/spring/games/monopoly/GamesControllerTest.java
@@ -16,23 +16,25 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GamesControllerTest {
 
 	@Autowired
 	private WebApplicationContext ctx;
 
+    @Value("${local.server.port}")
+    private int port;
 	private MockMvc mockMvc;
 
 	@Before
