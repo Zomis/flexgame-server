efda87d71b2779b46327f61a784b7dc6badecda1
Simon Forsberg <zomis2k@hotmail.com>
2017-01-07 11:34:30 +0100

    Add constructGame to GroovyGameHelper

--- monopoly-spring/src/main/java/net/zomis/spring/games/generic/GroovyGameHelper.java
+++ monopoly-spring/src/main/java/net/zomis/spring/games/generic/GroovyGameHelper.java
@@ -1,7 +1,36 @@
 package net.zomis.spring.games.generic;
 
-public class GroovyGameHelper implements GameHelper<C, G> {
+import net.zomis.spring.games.messages.GameMoveResult;
 
+import java.util.function.Function;
 
+public class GroovyGameHelper implements GameHelper<Object, Object> {
+
+    public Function<Object, Object> constructor;
+
+    @Override
+    public Object constructGame(Object configuration) {
+        return constructor.apply(configuration);
+    }
+
+    @Override
+    public void addPlayer(Object playerConfig) {
+
+    }
+
+    @Override
+    public Object start() {
+        return null;
+    }
+
+    @Override
+    public GameMoveResult performAction(int playerIndex, Object action) {
+        return null;
+    }
+
+    @Override
+    public Object gameDetails(Object game) {
+        return null;
+    }
 
 }
