4fbe677abe6f502ae0d1731bfb87377218a25f06
Simon Forsberg <zomis2k@hotmail.com>
2017-10-19 20:54:48 +0200

    Add documentation to GameHelper2

--- monopoly-spring/src/main/java/net/zomis/spring/games/generic/v2/GameHelper2.java
+++ monopoly-spring/src/main/java/net/zomis/spring/games/generic/v2/GameHelper2.java
@@ -13,6 +13,14 @@ public interface GameHelper2<G> {
     // TODO: Should this maybe throw an Exception? Or return ActionResult?
     LobbyGame<G> createGame(Object gameConfiguration);
     ActionResult playerJoin(LobbyGame<G> game, Object playerConfiguration);
+
+    /**
+     * Validate Lobby game (number of players specifically) and return created game
+     *
+     * @param game LobbyGame that wants to be started
+     * @return Game that was created, or null if game was not allowed to be started
+     */
+    // TODO: Use Either<G, String> or something to return failure status
     G startGame(LobbyGame<G> game);
     ActionResult performAction(RunningGame<G> game, PlayerInGame player, String actionType, Object actionData);
     Object gameSummary(RunningGame<G> game);
