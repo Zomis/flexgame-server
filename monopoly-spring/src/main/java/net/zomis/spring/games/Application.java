c86fe2e50d577dc38f0e0bd945386ec4829c5d8a
Simon Forsberg <zomis2k@hotmail.com>
2017-10-21 13:28:29 +0200

    Configure CORS

--- monopoly-spring/src/main/java/net/zomis/spring/games/Application.java
+++ monopoly-spring/src/main/java/net/zomis/spring/games/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -17,6 +20,16 @@ public class Application {
     @Bean
     public GroovyGames games() {
         return new GroovyGames();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://www.zomis.net");
+            }
+        };
     }
 
 }
