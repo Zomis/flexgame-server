d1eaffb502a5769010322d37e8c59a068d0c9686
Simon Forsberg <zomis2k@hotmail.com>
2017-10-29 12:21:41 +0100

    Add new CORS domain

--- flexgame-server/src/main/java/net/zomis/spring/games/Application.java
+++ flexgame-server/src/main/java/net/zomis/spring/games/Application.java
@@ -27,7 +27,7 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://www.zomis.net");
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://www.zomis.net", "http://gbg.zomis.net:8079");
             }
         };
     }
