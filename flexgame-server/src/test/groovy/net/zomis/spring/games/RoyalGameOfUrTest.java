cf8b0abce1e83b4c84eb8c07d6ff5642062dd1f7
Simon Forsberg <zomis2k@hotmail.com>
2017-11-14 19:39:40 +0100

    Add RoyalQLearn

--- flexgame-server/src/test/groovy/net/zomis/spring/games/RoyalGameOfUrTest.java
+++ flexgame-server/src/test/groovy/net/zomis/spring/games/RoyalGameOfUrTest.java
@@ -1,5 +1,6 @@
 package net.zomis.spring.games;
 
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import net.zomis.aiscores.FieldScoreProducer;
 import net.zomis.aiscores.FieldScores;
 import net.zomis.fight.FightInterface;
@@ -7,12 +8,14 @@ import net.zomis.fight.FightResults;
 import net.zomis.fight.GameFight;
 import net.zomis.spring.games.generic.PlayerInGame;
 import net.zomis.spring.games.generic.v2.ActionV2;
+import net.zomis.spring.games.impls.MyQLearning;
 import net.zomis.spring.games.impls.ur.RoyalGameOfUr;
 import net.zomis.spring.games.impls.ur.RoyalGameOfUrAIs;
 import net.zomis.spring.games.impls.ur.RoyalGameOfUrHelper;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static net.zomis.spring.games.impls.ur.RoyalGameOfUrAIs.*;
 import static org.junit.Assert.assertFalse;
@@ -66,11 +69,49 @@ public class RoyalGameOfUrTest {
         System.out.println(scores.getScores());
         move = ai.control(ur, new PlayerInGame("", 1, "", null, ai));
         assertEquals(6, move.get().getActionData().asInt());
+    }
+
+    public static void main(String[] args) {
+        RoyalGameOfUrHelper helper = new RoyalGameOfUrHelper();
+    }
+
+    private static class RoyalQLearn implements AI {
+
+        private final MyQLearning<RoyalGameOfUr, Long> learn;
+
+        public RoyalQLearn(MyQLearning<RoyalGameOfUr, Long> learn) {
+            this.learn = learn;
+        }
+
+        @Override
+        public Optional<ActionV2> control(RoyalGameOfUr game, PlayerInGame player) {
+            if (game.getCurrentPlayer() != player.getIndex()) {
+                return Optional.empty();
+            }
+            if (game.isRollTime()) {
+                return Optional.of(new ActionV2("roll", null));
+            }
+            if (!game.isMoveTime()) {
+                return Optional.empty();
+            }
+
+            int action = learn.pickAction(game);
+            return Optional.of(new ActionV2("move", new JsonNodeFactory(false).numberNode(action)));
+        }
 
     }
 
+
     @Test
     public void fight() {
+        MyQLearning.ActionPossible<RoyalGameOfUr> actionPossible = (environment, action) ->
+            environment.canMove(environment.getCurrentPlayer(), action, environment.getRoll());
+        MyQLearning<RoyalGameOfUr, Long> learn = new MyQLearning<>(15, RoyalGameOfUr::toLong, actionPossible, (state, action) -> state << 4 + action);
+        learn.setLearningRate(0.2);
+        learn.setDiscountFactor(0.99);
+        learn.setRandomMoveProbability(0.1);
+        RoyalQLearn royalQ = new RoyalQLearn(learn);
+
         GameFight<RoyalGameOfUrAIs.AI> fight = new GameFight<>("UR");
         RoyalGameOfUrAIs.AI[] ais = new RoyalGameOfUrAIs.AI[] {
                 new RoyalGameOfUrAIs.URScorer("KFE521S3", RoyalGameOfUrAIs.scf()
@@ -100,24 +141,53 @@ public class RoyalGameOfUrTest {
                         .withScorer(leaveSafety, -0.1)
                         .withScorer(riskOfBeingTaken, -3)
                         .withScorer(exit)),
+                royalQ,
         };
         RoyalGameOfUrHelper helper = new RoyalGameOfUrHelper();
+        JsonNodeFactory factory = new JsonNodeFactory(false);
+        MyQLearning.PerformAction<RoyalGameOfUr> performAction = (state, action) -> {
+            int currentPlayer = state.getCurrentPlayer();
+            helper.performAction(state, state.getCurrentPlayer(), "move", factory.numberNode(action));
+            while (!state.isFinished() && state.isRollTime()) {
+                state.roll();
+            }
+            int nextPlayer = state.getCurrentPlayer();
+
+            double reward = state.isFinished() ? state.getWinner() == currentPlayer ? 10 : -10 : -0.01;
+            double discountFactor = currentPlayer == nextPlayer ? learn.getDiscountFactor() : learn.getDiscountFactor() * -1;
+            return new MyQLearning.Rewarded<>(state, reward).withDiscountFactor(discountFactor);
+        };
         FightInterface<RoyalGameOfUrAIs.AI> strat = new FightInterface<RoyalGameOfUrAIs.AI>() {
             @Override
             public RoyalGameOfUrAIs.AI determineWinner(RoyalGameOfUrAIs.AI[] players, int fightNumber) {
                 RoyalGameOfUr ur = new RoyalGameOfUr();
+                while (ur.isRollTime()) {
+                    ur.roll();
+                }
                 while (!ur.isFinished()) {
+                    if (!ur.isMoveTime()) {
+                        throw new IllegalStateException("NOT MOVE TIME!");
+                    }
+
                     RoyalGameOfUrAIs.AI player = players[ur.getCurrentPlayer()];
                     Optional<ActionV2> result = player.control(ur, new PlayerInGame("", ur.getCurrentPlayer(), "", null, player));
                     // System.out.printf("Result for %d: %s. Values %s", ur.getCurrentPlayer(), result.orElse(null), Arrays.deepToString(ur.getPieces()));
-                    result.ifPresent(act -> helper.performAction(ur, ur.getCurrentPlayer(), act.getName(), act.getActionData()));
+                    result.ifPresent(act -> learn.step(ur, performAction, act.getActionData().asInt()));
                 }
-
                 return players[ur.getWinner()];
             }
         };
-        FightResults<RoyalGameOfUrAIs.AI> results = fight.fightEvenly(ais, 1000, strat);
-        System.out.println(results.toStringMultiLine());
+
+        int lastSize;
+        do {
+            lastSize = learn.getQTableSize();
+            FightResults<RoyalGameOfUrAIs.AI> results = fight.fightEvenly(ais, 1000, strat);
+            System.out.println(results.toStringMultiLine());
+            System.out.println("States: " + learn.getQTableSize());
+
+            int newStates = learn.getQTableSize() - lastSize;
+            System.out.println(newStates + " new states");
+        } while (lastSize != learn.getQTableSize());
     }
 
 }
