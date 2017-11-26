3638580a71a5048816b78b222dae4fcb84a6f9ef
Simon Forsberg <zomis2k@hotmail.com>
2017-11-26 12:02:53 +0100

    Add JavaFX view for XO-games

--- flexgame-server/src/main/java/net/zomis/spring/games/impls/qlearn/TTTQLearn.java
+++ flexgame-server/src/main/java/net/zomis/spring/games/impls/qlearn/TTTQLearn.java
@@ -10,24 +10,24 @@ import java.util.function.Function;
 public class TTTQLearn {
 
     public static final MyQLearning.PerformAction<TTController> performAction = (tt, action) -> {
-        int x = action % 3;
-        int y = action / 3;
+        int columns = tt.getGame().getSizeX();
+        int x = action % columns;
+        int y = action / columns;
         TTPlayer player = tt.getCurrentPlayer();
         tt.play(tt.getGame().getSub(x, y));
         double reward = 0;
 
         if (tt.isGameOver() && tt.getWonBy().isExactlyOnePlayer()) {
             reward = tt.getWonBy().is(player) ? 1 : -1;
-        } else if (isDraw(tt)) {
-            reward = 0;
         } else {
             reward = -0.01;
         }
         return new MyQLearning.Rewarded<>(tt, reward);
     };
     public static final MyQLearning.ActionPossible<TTController> actionPossible = (tt, action) -> {
-        int x = action % 3;
-        int y = action / 3;
+        int columns = tt.getGame().getSizeX();
+        int x = action % columns;
+        int y = action / columns;
         if (tt.getGame().getSub(x, y) == null) {
             return false;
         }
@@ -35,10 +35,17 @@ public class TTTQLearn {
     };
 
     public static MyQLearning<TTController, String> newLearner(QStore<String> qStore) {
-        int sizeX = 3;
-        int sizeY = 3;
+        return newLearner(9, qStore);
+    }
 
+    public static MyQLearning<TTController, String> newLearner(TTController controller, QStore<String> qStore) {
+        return newLearner(controller.getGame().getSizeX() * controller.getGame().getSizeY(), qStore);
+    }
+
+    private static MyQLearning<TTController, String> newLearner(int maxActions, QStore<String> qStore) {
         Function<TTController, String> stateToString = g -> {
+            int sizeX = g.getGame().getSizeX();
+            int sizeY = g.getGame().getSizeY();
             StringBuilder str = new StringBuilder();
             for (int y = 0; y < sizeY; y++) {
                 for (int x = 0; x < sizeX; x++) {
@@ -49,7 +56,7 @@ public class TTTQLearn {
             }
             return str.toString();
         };
-        MyQLearning<TTController, String> learn = new MyQLearning<>(9, stateToString, actionPossible,
+        MyQLearning<TTController, String> learn = new MyQLearning<>(maxActions, stateToString, actionPossible,
             (state, action) -> state + action, qStore);
         // learn.setLearningRate(-0.01); // This leads to bad player moves. Like XOX-OXO-_X_ instead of XOX-OXO-X__
         learn.setDiscountFactor(-0.9);
